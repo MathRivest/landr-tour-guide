de8f18720b08323b4043ffd30389e9be474c9341
Mathieu Rivest <rivest.mathieu@gmail.com>
2017-11-10 13:13:42 -0500

    initial empty component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Cities from './cities/cities';
+import Map from './map/map';
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+
+                <div className="App-body">
+                    <Cities />
+
+                    <Map />
+                </div>
             </div>
         );
     }
---
+++ src/cities/cities.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cities extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>List of cities goes here</div>;
+    }
+}
+
+export default Cities;
